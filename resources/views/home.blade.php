502814dbcf381b016e17e55edb3d04c11c80e7b8
Leonardo <126499051+Korreiia@users.noreply.github.com>
2023-03-30 20:33:17 -0300

    att

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible"content="IE=edge">
+    <meta name="viewport"content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style_home.css">
+    <title>SOPA</title>
+</head>
+<body>
+    <header>
+            <IMg class="logo" src="img/logo_alternativa.png" alt="fabião"></IMg>
+        <nav>
+            <div class="icon"><a href="http://#"target="_self"type="Empresa"hreflang="pt-br"><img class="icon" src="img/icone_home.png" alt=""> Home </a></div>
+            <div class="icon"><a href="http://#"target="_self"type="Produtos"hreflang="pt-br"><img class="icon" src="img/icon_inven.png" alt="">Inventário</a></div>
+            <div class="icon"><a href="http://#"target="_self"type="quem somos"hreflang="pt-br"><img class="icon" src="img/icon_hist.png" alt="">Histórico</a></div>
+            <div class="icon"><a href="FAQ.HTML"target="_self"type="FAQ"hreflang="pt-br"><img class="icon" src="img/icon_solicit.png" alt="">Solicitação</a></div>
+            <div class="icon"><a href="FAQ.HTML"target="_self"type="FAQ"hreflang="pt-br"><img class="icon" src="img/icon_calen.png" alt="">Calendário</a></div>
+            <div class="icon"><a href="FAQ.HTML"target="_self"type="FAQ"hreflang="pt-br"><img class="icon" src="img/icon_conf.png" alt="">Configurações</a></div>
+        </nav>
+    </header>
+    <main>
+        <div class="centro"></div>
+
+        <div class="clear"></div>
+
+        
+
+     </main>
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Tela de Login</title>
+    <link rel="stylesheet" href="CSS/style.css">
+  </head>
+  <body>
+    <div class="login-wrapper">
+      <div class="login">
+        <img class="logo" src="img/logo_alternativa.png" alt="fabião">
+        <form>
+          <label for="password">Senha:</label>
+          <input type="password" id="password" name="password" required>
+          <button type="submit">Entrar</button>
+          <div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
