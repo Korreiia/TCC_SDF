b2a5ad97a571f18313f3bbc5b1d4fc42a4ddc262
korreiia <korreiiaaa@gmail.com>
2023-05-06 00:40:16 -0300

    sei la po

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img class="logoh1" src="img/logoR.png" alt="logo">  
             <img class="logoh1" src="img/doa_tec.png" alt="logo">
             <div>
-                <a href="saibamais">Saiba Mais</a>
+                <a href="saibamais" class="saibamais">Saiba Mais</a>
 
                 <br>
 
