a0d7183bccc6b22ade49a409d9832950124fcead
lianccn <lianmarinheiro2005@gmail.com>
2023-05-05 22:15:50 -0300

    aaa fds

--- resources/views/inventario.blade.php
+++ resources/views/inventario.blade.php
@@ -1,46 +1,48 @@
 <x-layout>
 
 <link rel="stylesheet" href="/css/style_inventario.css">
-    
-<a class="criar_cadastro" href="/criar_inventario">Criar novo cadastro</a>
+    <div class="layout2">
+        <a class="criar_cadastro" href="/criar_inventario">Criar novo cadastro</a>
 
-<h1>Inventario</h1>
-        <table class="table">
-            <thead>
-            <br>
-                <tr class="tr">
-                    <th scope="col">ID</th>
-                    <th scope="col">Está Funcionando?</th>
-                    <th scope="col">Data de Entrada</th>
-                    <th scope="col">Descrição</th>
-                    <th scope="col">Estado de Conservação</th>
-                    <th scope="col">Quantidade</th>
-                    <th scope="col">...Ação...</th>
-                </tr>
-            </thead>
+        <h1>Inventario</h1>
+                <table class="table">
+                    
+                <thead>
+                    <br>
+                        <tr class="tr">
+                            <th class="th" scope="col">ID</th>
+                            <th class="th" scope="col">Está Funcionando?</th>
+                            <th class="th" scope="col">Data de Entrada</th>
+                            <th class="th" scope="col">Descrição</th>
+                            <th class="th" scope="col">Estado de Conservação</th>
+                            <th class="th" scope="col">Quantidade</th>
+                            <th class="th" scope="col">...Ação...</th>
+                        </tr>
+                    </thead>
 
-            <tbody>
-            @foreach($inventarios as $inventario)
-                <tr class="tr">
-                    <th>{{ $inventario ->id }}</th>
-                    <th>{{ $inventario ->estadofuncionamento }}</th>
-                    <th>{{ $inventario ->dataentrada }}</th>
-                    <th>{{ $inventario ->descricao }}</th>
-                    <th>{{ $inventario ->estadoconservacao }}</th>
-                    <th>{{ $inventario ->quantidade }}</th>
-                    <th>
-                        <a href="{{ route('editarInventario', ['id'=>$inventario->id]) }}">Editar</a>
+                    <tbody>
+                    @foreach($inventarios as $inventario)
+                        <tr class="tr">
+                            <th>{{ $inventario ->id }}</th>
+                            <th>{{ $inventario ->estadofuncionamento }}</th>
+                            <th>{{ $inventario ->dataentrada }}</th>
+                            <th>{{ $inventario ->descricao }}</th>
+                            <th>{{ $inventario ->estadoconservacao }}</th>
+                            <th>{{ $inventario ->quantidade }}</th>
+                            <th>
+                                <a href="{{ route('editarInventario', ['id'=>$inventario->id]) }}">Editar</a>
 
-                        <form action="{{ route('deletarInventario', ['id'=>$inventario->id]) }}" method="post">
-                        
-                        @csrf 
-                        @method('delete')
-                        <button type="submit">Deletar</button>
+                                <form action="{{ route('deletarInventario', ['id'=>$inventario->id]) }}" method="post">
 
-                        </form>
+                                @csrf 
+                                @method('delete')
+                                <button type="submit">Deletar</button>
 
-                    </th>
-                </tr>
+                                </form>
+
+                            </th>
+                        </tr>
+    </div>
             @endforeach
             </tbody>
 
