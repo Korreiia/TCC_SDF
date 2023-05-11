04743b8cd9891fd671669a47e73db161794812ff
korreiia <korreiiaaa@gmail.com>
2023-05-11 01:03:27 -0300

    tem coisa errada ae

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,26 @@
     <x-layout>
-        <div class="layout"><!--parte das abas(navegação) -->
+        <div class="layout"><!--parte das abas(navegação) (Oi ??????????) -->
             <link rel="stylesheet" href="/css/style_home.css">
     
         </div>
         <div class="layout2"> <!--conteudo-->  
-            <img class="logoh1" src="img/logoR.png" alt="logo">  
+            {{-- <img class="logoh1" src="img/logoR.png" alt="logo">   --}}
             <div>
                 <a href="saibamais" class="saibamais">Saiba Mais</a>
             
-
                 <br>
-                
-
+            
                 <a href="login" class="login">Faça uma conta</a>
-
-                <pre>
-                         O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
-                         que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
-                         com o intuito de reclicar esses quipamentos,
-                          para fazer novos computadores para pessoas sem condicao.
-                </pre>
-
-                <img class="logoh1" src="img/logoR.png" alt="logo">  
-                
             </div>
+
+            <pre>
+O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
+que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
+com o intuito de reclicar esses quipamentos,
+para fazer novos computadores para pessoas sem condicao.
+            </pre>
+            <img class="logoh1" src="img/logoR.png" alt="logo">
+
         </div>
     </x-layout>
     
