8315e8177dd0b3dfec4d1a1e4ce669a84cb19ffe
lianccn <lianmarinheiro2005@gmail.com>
2023-05-12 22:22:51 -0300

    a tela de configuração ta uma maravilha

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -8,13 +8,14 @@
         <h1>Dados Pessoais</h1>
         <form action="{{ route('atualizarUsuario3',['id'=>$usuarios->id]) }}" method="post">
         @method('put')
-            <input type="text" name="nome" value="{{ $usuarios->nome }}" placeholder="Nome" required>
-            <input type="number" name="cpf" value="{{ $usuarios->cpf }}" placeholder="CPF" required>
-            <input type="number" name="telefone" value="{{ $usuarios->telefone }}" placeholder="Telefone/Celular" required>
-            <input type="text" name="endereco" value="{{ $usuarios->endereco }}" placeholder="Endereço" required>
-        <hr>
-            <button type="submit">Atualizar Dados</button>
+            <input class="dados" type="text" name="nome" value="{{ $usuarios->nome }}" placeholder="Nome" required>
+            <input class="dados" type="number" name="cpf" value="{{ $usuarios->cpf }}" placeholder="CPF" required>
+            <input class="dados" type="number" name="telefone" value="{{ $usuarios->telefone }}" placeholder="Telefone/Celular" required>
+            <input class="dados" type="text" name="endereco" value="{{ $usuarios->endereco }}" placeholder="Endereço" required>
+        <br>
+            <button class="dados" type="submit">Atualizar Dados</button>
             @csrf
+        <hr>    
         </form>
 
         <br>
@@ -22,10 +23,11 @@
         <h1>Atualizar E-email</h1>
         <form action="{{ route('atualizarUsuario1',['id'=>$usuarios->id]) }}" method="post">
         @method('put')
-            <input type="email" name="email" placeholder="E-mail" required>
-        <hr>
-            <button type="submit">Atualizar E-mail</button>
+            <input class="dados" type="email" name="email" placeholder="E-mail" required>
+        <br>
+            <button class="dados" type="submit">Atualizar E-mail</button>
             @csrf
+        <hr>
         </form>
 
         <br>
@@ -33,11 +35,12 @@
         <h1>Atualizar Senha</h1>
         <form action="{{ route('atualizarUsuario2',['id'=>$usuarios->id]) }}" method="post">
         @method('put')
-            <input type="password" name="senha" placeholder="Senha" required>
-            <input type="password" name="senha1" placeholder="Repita a senha" required>
-        <hr>
-            <button type="submit">Atualizar Senha</button>
+            <input class="dados" type="password" name="senha" placeholder="Senha" required>
+            <input class="dados" type="password" name="senha1" placeholder="Repita a senha" required>
+        <br>
+            <button class="dados" type="submit">Atualizar Senha</button>
             @csrf
+        <hr>    
         </form>
 
         <br>
@@ -46,7 +49,7 @@
         <form action="{{ route('deletarUsuario', ['id'=>$usuarios->id]) }}" method="post">           
             @csrf 
             @method('delete')
-            <button type="submit">Deletar</button>
+            <button class="dados" type="submit">Deletar</button>
         </form>
     </div>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,14 @@
                 <br>
                 <a href="login" class="login">Faça uma conta</a>
             </div>
-
-            <a href="saibamais" class="saibamais1">Saiba Mais</a>
-
-            <pre class="resumo">
-O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
-que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
-com o intuito de reclicar esses quipamentos,
-para fazer novos computadores para pessoas sem condicao.
-            </pre>
+            <div>
+                <pre class="resumo">
+                    O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
+                    que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
+                    com o intuito de reclicar esses quipamentos,
+                    para fazer novos computadores para pessoas sem condicao.
+                </pre>
+            </div>
             <img class="logoh1" src="img/logoR.png" alt="logo">
 
         </div>
