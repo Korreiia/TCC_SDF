f9aa414673dd791daf856a8e62a126fb05ad98de
Pedro <pedrosilvalima908@gmail.com>
2023-05-15 22:24:05 -0300

    mudanças home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,23 @@
         <div class="layout2"> <!--conteudo-->  
             {{-- <img class="logoh2" src="img/logoR.png" alt="logo">   --}}
             <div class="botoes">
-                <a href="saibamais" class="saibamais">Saiba Mais</a>
                 <br>
-                <a href="criar_conta" class="criar_conta">Faça uma conta</a>
+                
             </div>
             
-            <div>
+            
                 <pre class="resumo">
                     O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
                     que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
                     com o intuito de reclicar esses quipamentos,
                     para fazer novos computadores para pessoas sem condicao.
                 </pre>
-            </div>
+            
             <img class="logoh1" src="img/logoR.png" alt="logo">
+            <img class="logoh3" src="img/logoR.png" alt="logo">
+            <a href="saibamais" class="saibamais">Saiba Mais</a>
+            <pre class="pre_botao">Já tem uma conta?</pre>
+            <a href="criar_conta" class="criar_conta">Faça uma conta</a>
 
         </div>
     </x-layout>
