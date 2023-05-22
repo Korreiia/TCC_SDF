35b992797894cb7f0e7fffdd6458f32c62d6574b
korreiia <korreiiaaa@gmail.com>
2023-05-22 19:36:29 -0300

    b

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,20 @@
     <x-layout>
         <link rel="stylesheet" href="/css/style_home.css">
-        
-        <div class="layout2"> <!--conteudo-->  
-            {{-- <img class="logoh2" src="img/logoR.png" alt="logo">   --}}
-            <div class="botoes">
-                <br>
-                
-            </div>
             
                 <pre class="resumo">
-                    O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
-                    que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
-                    com o intuito de reclicar esses quipamentos,
-                    para fazer novos computadores para pessoas sem condicao.
+O ReciclaEtec é um projeto desenvolvido e coordenado pela Etec, 
+que tem como principal objetivo receber doaçoes de peças e equipamentos velhos,
+com o intuito de reclicar esses quipamentos,
+para fazer novos computadores para pessoas sem condicao.
                 </pre>
             
-            <img class="logoh1" src="img/reciclaetecN.png" alt="logo">
             <img class="logoh3" src="img/logoR.png" alt="logo">
             <a href="saibamais" class="saibamais">Saiba Mais</a>
-            <pre class="pre_botao">Já tem uma conta?</pre>
+
+            <p class="p_botao1">Já tem uma conta?</p>
+            <a href="login" class="login">Faça Login</a>
+
+            <p class="p_botao2">Não tem uma conta ainda?</p>
             <a href="criar_conta" class="criar_conta">Faça uma conta</a>
 
 </x-layout>
